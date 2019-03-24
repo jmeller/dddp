fb91ab58660688ba1a0a2cb831713ca4c781e4f4
Jan <jan.meller@web.de>
2019-03-24 16:44:23 +0100

    Finished the application and uploaded it to shinyapps.io

--- course_project/dddp_course_project/server.R
+++ course_project/dddp_course_project/server.R
@@ -1,26 +1,65 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# This is the server logic of a Shiny web application. 
 #
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+  
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # switch data sets based on input data
+    switch(input$data_set, 
+           "mtcars" = {
+             xlab <- "Miles per Gallon"
+             plot_data <- data.frame(x = mtcars$mpg)
+           },
+           "rivers" = {
+             xlab <- "Lengths of Major North American Rivers (in miles)"
+             plot_data <- data.frame(x = rivers)
+           },
+           "discoveries" = {
+             xlab <- "Yearly Numbers of Important Discoveries"
+             plot_data <- data.frame(x = as.numeric(discoveries))
+           })
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    plot_data %>% ggplot(aes(x = x)) +
+      geom_histogram(aes(y = ..density..), bins = input$bins) + 
+      theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank()) +
+      labs(x = xlab, y = "Frequency") +
+      ggtitle(paste("Histogram of ", xlab)) -> plot
     
+    # add distributions
+    mean <- mean(plot_data$x)
+    sd <- sd(plot_data$x)
+    n <- nrow(plot_data)
+    min <- min(plot_data$x)
+    max <- max(plot_data$x)
+    
+    if(input$dnorm){
+      y_norm <- dnorm(plot_data$x, mean, sd)
+      plot <- plot + geom_line(aes(x = plot_data$x, y = y_norm),
+                               size = 1.4, color = "red")
+    }
+    if(input$dexp){
+      y_exp <- dexp(plot_data$x, rate = 1/mean)
+      plot <- plot + geom_line(aes(x = plot_data$x, y = y_exp),
+                               size = 1.4, color = "blue")
+    }
+    if(input$dunif){
+      y_unif <- dunif(plot_data$x, min = min, max = max)
+      plot <- plot + geom_line(aes(x = plot_data$x, y = y_unif),
+                               size = 1.4, color = "green")
+    }
+    
+    plot
   })
   
 })
--- course_project/dddp_course_project/ui.R
+++ course_project/dddp_course_project/ui.R
@@ -2,10 +2,6 @@
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
@@ -13,21 +9,52 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Analysis of distributional assumptions"),
+  
+  # Description
+  h2("Goal"),
+  p(paste0("This application visualizes the histograms of variables ", 
+           "from different data sets. Moreover, you can visually inspect the fit ",
+           "of different standard distributions.")),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      # Select data set
+      h3("Parameters"),
+      strong("Data set:"),
+      p("Please choose the data to visualize among the following three data sets."),
+      selectInput("data_set", NULL,
+                  c("Motor Trend Car Road Tests" = "mtcars",
+                    "Lengths of Major North American Rivers" = "rivers",
+                    "Yearly Numbers of Important Discoveries" = "discoveries")
+                  
+      ),
+      # Select number of bins
+      strong("Number of bins:"),
+      p(paste("Now you can choose the number of bins the data is split up into for",
+        "the histogram.")),
+      sliderInput("bins",
+                  NULL,
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      
+      strong("Distribution:"),
+      p(paste("By checking one of the following distributions, the respective", 
+              "distribution will be fitted to the displayed data.")),
+      checkboxInput("dnorm", "Normal distribution", FALSE),
+      checkboxInput("dexp", "Exponential distribution", FALSE),
+      checkboxInput("dunif", "Uniform distribution", FALSE)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      h3("Results"),
+      p(paste("In this area, the chosen data as well as the chosen density",
+              "distributions will be plotted so you can inspect the fit of the",
+              "respective distribution to the data.")),
+      plotOutput("distPlot")
     )
   )
 ))
